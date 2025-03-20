4fed195d1d76ad2924ccaad46179becdf1d7bd04
discaptive <discaptive@gmail.com>
2025-03-20 18:37:10 +0900

    Add `rel="noreferrer"` to all `target="_blank"` links

--- build.js
+++ build.js
@@ -24,6 +24,17 @@ const marked = new Marked(
   })
 );
 
+const renderer = {
+  link(href, title, text) {
+    const link = marked.Renderer.prototype.link.call(this, href, title, text);
+    return link.replace("<a", "<a target='_blank' rel='noreferrer' ");
+  },
+};
+
+marked.use({
+  renderer,
+});
+
 function loadTemplate(template) {
   return fs.readFileSync(path.join(TEMPLATE_DIR, `${template}.html`), "utf-8");
 }
